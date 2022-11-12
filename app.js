34ba06220e8ecffba69bf300ba04f49a96eb4f7c
masterMav <mavdoesdev@gmail.com>
2022-11-12 16:18:22 +0530

    repeated entries removal

--- app.js
+++ app.js
@@ -46,48 +46,61 @@ app.get("/users/:handle/:invalidUrl", (req, res) => {
   const API_URI = `https://codeforces.com/api/user.status?handle=${handle}&lang=en`;
   const subMap = new Map();
 
-  async function getUserSubmissions() {
-    const response = await fetch(API_URI);
-    const tmp = await response.json();
+  const checkFetch = (response) => {
+    if (!response.ok) throw Error("CF API ERROR");
+    return response;
+  };
 
-    tmp.result.forEach((i) => {
-      const str = `${i.problem.index}. ${i.problem.name}`;
-      let prvsVerdict, newVerdict;
-      if (subMap.get(str) === undefined) prvsVerdict = 0;
-      else prvsVerdict = subMap.get(str);
+  fetch(API_URI)
+    .then(checkFetch)
+    .then((response) => response.json())
+    .then((data) => {
+      data.result.forEach((i) => {
+        const str = `${i.problem.index}. ${i.problem.name}`;
+        let prvsVerdict, newVerdict;
+        if (subMap.get(str) === undefined) prvsVerdict = 0;
+        else prvsVerdict = subMap.get(str);
 
-      if (i.verdict === "OK") newVerdict = 2;
-      else newVerdict = 1;
+        if (i.verdict === "OK") newVerdict = 2;
+        else newVerdict = 1;
 
-      subMap.set(str, Math.max(newVerdict, prvsVerdict));
+        subMap.set(str, Math.max(newVerdict, prvsVerdict));
+      });
+
+      //Render userDashboard with qns
+      User.find({ handle })
+        .then((results) => {
+          // qns is an array of objects with _id, problemName & verdict
+          const qns = results.map((result) => {
+            let updatedVerdict;
+            if (subMap.get(result.problemName) === undefined)
+              updatedVerdict = 0;
+            else updatedVerdict = subMap.get(result.problemName);
+
+            return {
+              _id: result._id,
+              problemName: result.problemName,
+              verdict: updatedVerdict,
+            };
+          });
+
+          res.render("dashboard", {
+            title: "Dashboard",
+            qns,
+            handle,
+            invalidUrl,
+          });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => {
+      res.render("dashboard", {
+        title: "Dashboard",
+        qns: [],
+        handle,
+        invalidUrl: "2",
+      });
     });
-
-    //Render userDashboard with qns
-    User.find({ handle })
-      .then((results) => {
-        // qns is an array of objects with _id & problemName
-        const qns = results.map((result) => {
-          let updatedVerdict;
-          if (subMap.get(result.problemName) === undefined) updatedVerdict = 0;
-          else updatedVerdict = subMap.get(result.problemName);
-
-          return {
-            _id: result._id,
-            problemName: result.problemName,
-            verdict: updatedVerdict,
-          };
-        });
-
-        res.render("dashboard", {
-          title: "Dashboard",
-          qns,
-          handle,
-          invalidUrl,
-        });
-      })
-      .catch((err) => console.log(err));
-  }
-  getUserSubmissions();
 });
 
 // POST requests
@@ -98,17 +111,27 @@ app.post("/users/:handle", (req, res) => {
       const $ = cheerio.load(html);
       const ps = $(".problem-statement .header .title");
 
-      // Save in DB & redirect
-      const entry = new User({
-        handle: req.params.handle,
-        problemName: ps.text(),
-      });
+      User.findOne({ handle: req.params.handle, problemName: ps.text() })
+        .then((result) => {
+          if (result === null) {
+            // Save in DB & redirect
+            const entry = new User({
+              handle: req.params.handle,
+              problemName: ps.text(),
+            });
 
-      entry
-        .save()
-        .then((result) => res.redirect(`${req.url}/0`))
+            entry
+              .save()
+              .then((result) => res.redirect(`${req.url}/0`))
+              .catch((err) => console.log(err));
+          } else {
+            //Problem Already Exists in DB
+            res.redirect(`${req.url}/3`);
+          }
+        })
         .catch((err) => console.log(err));
     } else {
+      //INVALID URL
       res.redirect(`${req.url}/1`);
     }
   });
